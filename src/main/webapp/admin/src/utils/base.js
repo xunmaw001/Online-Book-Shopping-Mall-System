28a56b7cc25890e53a8bedf685ddc980e36472f9
yulinyu <2508000341@QQ.COM>
2025-02-06 21:07:31 +0800

    1

---
+++ src/main/webapp/admin/src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+                return {
+            url : "http://localhost:8080/ssm57oqs/",
+            name: "ssm57oqs",
+            // 退出到首页链接
+            indexUrl: 'http://localhost:8080/ssm57oqs/front/index.html'
+        };
+            },
+    getProjectName(){
+        return {
+            projectName: "在线购书商城系统"
+        } 
+    }
+}
+export default base
